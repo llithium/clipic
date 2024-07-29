a11c1b8c3c4f177245435689ff3ce2ae2baa6d84
Nigel <barrowjr0@gmail.com>
2024-07-28 22:39:33 -0400

    Add double click play/pause

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { invoke } from "@tauri-apps/api";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
 import { Icon } from "@iconify/react";
@@ -13,8 +13,9 @@ function Index() {
   const [currentFileList, setCurrentFileList] = useState<string[]>([]);
   const [currentVideo, setCurrentVideo] = useState<string>();
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState<boolean>();
-  const video = document.querySelector("#videoElement") as HTMLVideoElement;
+  const [isPlaying, setIsPlaying] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   async function handleFiles() {
     if (currentFileList.length === 0) {
       const fileList: string[] = await invoke("open_file_picker");
@@ -25,14 +26,34 @@ function Index() {
     setCurrentIndex((prevIndex) => prevIndex + 1);
   }
   function handlePlayPause() {
-    if (video.paused) {
-      video.play();
-      setIsPlaying(true);
-    } else {
-      video.pause();
-      setIsPlaying(false);
+    const video = videoRef.current;
+    if (video) {
+      if (!video.paused) {
+        video.pause();
+        setIsPlaying(false);
+      } else {
+        video.play();
+        setIsPlaying(true);
+      }
     }
   }
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.key === " ") {
+      event.preventDefault();
+      handlePlayPause();
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("dblclick", handlePlayPause);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("dblclick", handlePlayPause);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   useEffect(() => {
     currentFileList &&
       setCurrentVideo(convertFileSrc(currentFileList[currentIndex]));
@@ -42,13 +63,10 @@ function Index() {
     <div className="w-full h-full">
       {currentFileList?.length > 0 ? (
         <>
-          <div
-            className="absolute w-full h-full z-50"
-            onClick={handlePlayPause}
-          >
-            <div className="absolute bottom-2 w-full h-fit">
+          <div className="absolute w-full h-full z-50">
+            <div className="absolute bottom-2 w-full h-fit z-50">
               <Button
-                className="bg-transparent rounded-full"
+                className="bg-transparent rounded-full z-50"
                 onClick={handlePlayPause}
               >
                 {isPlaying ? (
@@ -61,7 +79,7 @@ function Index() {
           </div>
 
           <video
-            id="videoElement"
+            ref={videoRef}
             className="w-full h-full absolute z-0"
             // controls
             autoPlay
