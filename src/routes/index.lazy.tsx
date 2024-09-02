16616da0980e9ae9e4df2ca0861e943b47103a2b
Nigel <barrowjr0@gmail.com>
2024-09-02 17:09:31 -0400

    Style side panel

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -519,16 +519,22 @@ function Index() {
           </ResizablePanel>
           <ResizableHandle className="relative z-50 w-[3px] h-[calc(100vh-32px)] translate-y-8" />
           <ResizablePanel
-            className="flex flex-col justify-end relative z-40"
+            className="flex flex-col justify-end"
             defaultSize={25}
           >
-            <div className="h-[calc(100vh-32px)] dark:bg-neutral-50">
+            <div className="h-[calc(100vh-32px)] dark:bg-neutral-50 relative z-40 py-2 flex flex-col gap-1 overflow-y-auto">
               {currentFileList.map((video, i) => {
                 return (
-                  <div>
-                    <span onClick={() => updateCurrentIndex(i)} key={i}>
-                      {video.fileName}
-                    </span>
+                  <div
+                    key={i}
+                    className={`hover:opacity-80 cursor-pointer active:opacity-60 transition-all px-2 ${currentIndex === i && "bg-neutral-400/20"}`}
+                  >
+                    <div
+                      className="text-xs overflow-hidden overflow-ellipsis whitespace-nowrap"
+                      onClick={() => updateCurrentIndex(i)}
+                    >
+                      <span>{i + 1}. </span> {video.fileName}
+                    </div>
                   </div>
                 );
               })}
